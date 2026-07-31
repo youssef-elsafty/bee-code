3b951d3c728b00761845bef9cdd4a45762027e92
Youssef Ahmed Mohamed Hussien Elsafty <youssef.esafty5678@gmail.com>
2026-07-31 08:57:37 -0700

    Fix entrypoint for production

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collect static files"
 python manage.py collectstatic --settings=config.settings.production --noinput
 
 echo "Starting server"
-exec "$@"
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000}
