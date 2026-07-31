9ad4bb7a6334aa17b40b818a06f859bcec47f8a7
Youssef Ahmed Mohamed Hussien Elsafty <youssef.esafty5678@gmail.com>
2026-07-31 08:53:34 -0700

    Run migrations on startup in production

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
+
 echo "Apply database migrations"
-python manage.py migrate
+python manage.py migrate --settings=config.settings.production --noinput
 
 echo "Collect static files"
-python manage.py collectstatic --noinput
+python manage.py collectstatic --settings=config.settings.production --noinput
 
 echo "Starting server"
 exec "$@"
